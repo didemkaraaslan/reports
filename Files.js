de1cebe4993f958b0223ef757a717bc6010e97cc
Ashley20 <kucukkaraaslan.didem@gmail.com>
2019-07-25 15:33:19 +0300

    Final steps

---
+++ Files.js
@@ -0,0 +1,7 @@
+const countsInputFileUrl = "http://assignment.envanterium.com/inputs/counts.json";
+const masterInputFileUrl = "http://assignment.envanterium.com/inputs/master.json";
+
+module.exports = {
+  countsInputFileUrl,
+  masterInputFileUrl
+};
